ef6aac7968e4ea694d8aaf514c561629aeb7a155
Pawel Godula <bolomaster7@gmail.com>
2023-05-04 23:34:13 +0200

    class validator & second endpoit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,25 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
+import * as cors from 'cors';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
-import { OrdersService } from './orders/orders.service';
-import { OrdersController } from './orders/orders.controller';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [ProductsModule],
-  controllers: [AppController, OrdersController],
-  providers: [AppService, OrdersService],
+  imports: [ProductsModule, OrdersModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(cors()).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
