ea632ab93d1a88827069b9101b3e985b04fd4067
Pawel Godula <bolomaster7@gmail.com>
2023-05-04 18:58:33 +0200

    new endpoints

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Delete } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
@@ -9,4 +9,16 @@ constructor(private productsService: ProductsService) {}
 getAll(): any {
   return this.productsService.getAll();
 }
+
+@Get('/:id')
+public getById(@Param('id') id: string) {
+  return this.productsService.getById(id);
+}
+
+@Delete('/:id')
+deleteById(@Param('id') id: string) {
+  this.productsService.deleteById(id);
+  return { success: true };
+}
+
 }
