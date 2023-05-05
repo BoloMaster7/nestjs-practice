fe4c6889dd89d5ef5c6d181dd0e7efeaad004aeb
Pawel Godula <bolomaster7@gmail.com>
2023-05-05 14:44:12 +0200

    get by id, modified

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { OrdersService } from './orders.service';
+import { ParseUUIDPipe, NotFoundException } from '@nestjs/common';
 
 @Controller('orders')
 export class OrdersController {
@@ -9,9 +10,11 @@ export class OrdersController {
   getAll(): any {
     return this.ordersService.getAll();
   }
-
-  @Get('/:id')
-  getById(@Param('id') id: string) {
-    return this.ordersService.getById(id);
-  }
+  
+@Get('/:id')
+getById(@Param('id', new ParseUUIDPipe()) id: string) {
+  const ord = this.ordersService.getById(id);
+  if (!ord) throw new NotFoundException('Order not found');
+  return ord;
+}
 }
