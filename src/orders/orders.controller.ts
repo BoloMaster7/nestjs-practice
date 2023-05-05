f1a4cffa8c9bfad2d8a33d9e3224c1ffe52bf4dd
Pawel Godula <bolomaster7@gmail.com>
2023-05-05 15:16:42 +0200

    add orders & api

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Delete, Post, Put, Body } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { ParseUUIDPipe, NotFoundException } from '@nestjs/common';
+import { CreateOrderDTO } from 'src/orders/dtos/create-order.dto';
+import { UpdateOrderDTO } from 'src/orders/dtos/update-order.dto';
+
 
 @Controller('orders')
 export class OrdersController {
@@ -17,4 +20,28 @@ getById(@Param('id', new ParseUUIDPipe()) id: string) {
   if (!ord) throw new NotFoundException('Order not found');
   return ord;
 }
+
+@Delete('/:id')
+deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
+  if (!this.ordersService.getById(id))
+    throw new NotFoundException('Order not found');
+  this.ordersService.deleteById(id);
+  return { success: true };
+}
+@Post('/')
+  create(@Body() orderData: CreateOrderDTO) {
+    return this.ordersService.create(orderData);
+}
+
+@Put('/:id')
+  update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() orderData: UpdateOrderDTO,
+  ) {
+    if (!this.ordersService.getById(id))
+      throw new NotFoundException('Order not found');
+
+    this.ordersService.updateById(id, orderData);
+    return { success: true };
+  }
 }
