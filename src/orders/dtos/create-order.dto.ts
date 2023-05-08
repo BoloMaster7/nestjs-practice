38767c490afb63cd191b68498226354b4092ed77
Pawel Godula <bolomaster7@gmail.com>
2023-05-08 18:48:23 +0200

    prisma seed

--- src/orders/dtos/create-order.dto.ts
+++ src/orders/dtos/create-order.dto.ts
@@ -1,10 +1,12 @@
-import { IsNotEmpty, IsUUID } from 'class-validator';
+import { IsNotEmpty, IsUUID, IsString } from 'class-validator';
 
 export class CreateOrderDTO {
   @IsNotEmpty()
   @IsUUID()
+  @IsString()
   productId: string;
 
   @IsUUID()
+  @IsString()
   clientId: string;
 }
